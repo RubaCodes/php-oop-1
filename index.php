cb656cdac337af9bf8753a132fd6a848a40183eb
rubacodes <rubacodes@gmail.com>
2022-06-27 15:17:05 +0200

    Aggiunti commenti e variabili pubbliche

--- index.php
+++ index.php
@@ -6,27 +6,43 @@ class Movie
     public $actors = [];
     public $genres = [];
 
-
+    //costruttore
     function __construct($_title, $_originLanguage)
     {
         $this->titleSetter($_title);
         $this->originLanguageSetter($_originLanguage);
     }
-
+    //setter-getter titolo
     public function titleSetter($_title)
     {
         $this->title = $_title;
     }
+    public function titleGetter()
+    {
+        return $this->title;
+    }
+    //setter-getter lingua originale
     public function originLanguageSetter($_originLanguage)
     {
         $this->originLanguage = $_originLanguage;
     }
+    public function originLanguageGetter()
+    {
+        return $this->originLanguage;
+    }
 }
 
 $movies = [];
-
+//creazione oggetti movie
 $movies[] = new Movie("Harry Potter", "English");
 $movies[] = new Movie("La Vita e' Bella", "Italiano");
+//set sulle proprieta' pubbliche
+$movies[0]->genres = ['Avventura', 'Famiglia', 'Fiction'];
+$movies[1]->genres = ['Drammatica', 'Guerra', 'Romantico'];
+
+$movies[0]->authors = ['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'];
+$movies[1]->authors = ['Sergio Bini', 'Giorgio Cantarini', 'Roberto Benigni'];
+
 
 
 ?>
@@ -44,7 +60,8 @@ $movies[] = new Movie("La Vita e' Bella", "Italiano");
 
     <pre>
     <?php
-    var_dump($movies); ?>
+    var_dump($movies);
+    ?>
 </pre>
 </body>
 
