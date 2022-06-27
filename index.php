92329a97459039725b66f36c57bc06c109f60e02
rubacodes <rubacodes@gmail.com>
2022-06-27 15:45:28 +0200

    push finale

--- index.php
+++ index.php
@@ -53,6 +53,9 @@ $movies[1]->actors = ['Sergio Bini', 'Giorgio Cantarini', 'Roberto Benigni'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>OOP test</title>
 </head>
@@ -62,14 +65,14 @@ $movies[1]->actors = ['Sergio Bini', 'Giorgio Cantarini', 'Roberto Benigni'];
         <?php foreach ($movies as $movie) { ?>
             <div class="card">
                 <h1 class="text-center"><?php echo $movie->titleGetter() ?></h1>
-                <h3 class="text-center"><?php echo $movie->originLanguageGetter() ?></h3>
-                <h4 class="text-center">Attori principali</h4>
+                <h2 class="text-center"><?php echo $movie->originLanguageGetter() ?></h2>
+                <h3 class="text-center">Attori principali</h3>
                 <ul>
                     <?php foreach ($movie->actors as $actor) { ?>
                         <li><?php echo $actor ?></li>
                     <?php } ?>
                 </ul>
-                <h4 class="text-center">Generi</h4>
+                <h3 class="text-center">Generi</h3>
                 <ul>
                     <?php foreach ($movie->genres as $genre) { ?>
                         <li><?php echo $genre ?></li>
